9cd614ab4cc35152106dafc473a16096c7e175e0
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-28 23:51:22 +0430

    feat: Add `compact` method (#13)

---
+++ src/compact.ts
@@ -0,0 +1,24 @@
+/**
+ * It will filter out the falsey values from the provided `array`.
+ * @param array
+ * @example
+ * compact([0, 1, false, 2, '', 3, 'a', 'e' * 23, NaN, 's', 34]);
+ * // => [ 1, 2, 3, 'a', 's', 34 ]
+ */
+export default function compact<Value>(array: Value[]): Array<CompactT<Value>> {
+  return array.filter(Boolean) as Array<CompactT<Value>>;
+}
+
+export type CompactT<Value> = Value extends ""
+  ? never
+  : Value extends 0
+    ? never
+    : Value extends false
+      ? never
+      : Value extends null
+        ? never
+        : Value extends undefined
+          ? never
+          : Value extends typeof NaN
+            ? never
+            : Value;
