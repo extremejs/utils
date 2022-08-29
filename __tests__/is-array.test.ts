56d3f5f9fdf9a9aa2c9ed62e54cdbaede4407a2c
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-29 16:01:22 +0430

    test: Improve `isArray` tests

--- __tests__/is-array.test.ts
+++ __tests__/is-array.test.ts
@@ -3,5 +3,9 @@ import { isArray } from "@extremejs/utils";
 test("should determine whether the value is an array or not", () => {
   expect(isArray([])).toBe(true);
 
+  expect(isArray(Array([]))).toBe(true);
+
+  expect(isArray(new Array([]))).toBe(true);
+
   expect(isArray({})).toBe(false);
 });
