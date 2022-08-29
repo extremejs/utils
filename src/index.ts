e4688bb755ed90f92d0b4187e8563905f463932d
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-29 16:36:22 +0430

    feat: Add `isSymbol` method (#21)

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { default as isBoolean } from "./is-boolean.js";
 export { default as isEven } from "./is-even.js";
 export { default as isNumber } from "./is-number.js";
 export { default as isString } from "./is-string.js";
+export { default as isSymbol } from "./is-symbol.js";
 export { default as sum } from "./sum.js";
 export { default as sumByFn } from "./sum-by-fn.js";
---
+++ src/is-symbol.ts
@@ -0,0 +1,14 @@
+/**
+ * It will determine whether the provided `value` is a symbol or not.
+ * @since 1.0.0
+ * @param value
+ * @example
+ * isSymbol("foo");
+ * // => false
+ * @example
+ * isSymbol(Symbol());
+ * // => true
+ */
+export default function isSymbol(value: unknown): value is symbol {
+  return typeof value === "symbol";
+}
