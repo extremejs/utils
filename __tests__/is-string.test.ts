167325db3369c4fccec6f8810bf97cf4da8ee0f1
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-29 15:24:27 +0430

    feat: Add `isString` method (#17)

---
+++ __tests__/is-string.test.ts
@@ -0,0 +1,11 @@
+import { isString } from "@extremejs/utils";
+
+test("should determine whether the provided value is a string or not", () => {
+  expect(isString("foo")).toBe(true);
+
+  expect(isString(String("foo"))).toBe(true);
+
+  expect(isString(false)).toBe(false);
+
+  expect(isString(3)).toBe(false);
+});
