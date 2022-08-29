e89fd0c2efa54e92e1605840704bf2eb42830698
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-29 16:50:06 +0430

    feat: Add `toPath` method

---
+++ __tests__/to-path.test.ts
@@ -0,0 +1,13 @@
+import { toPath } from "@extremejs/utils";
+
+test("should return the path of a value in an object representing the property", () => {
+  expect(toPath("a.b[0].c[d]")).toEqual(["a", "b", "0", "c", "d"]);
+
+  expect(toPath("a.b.0.c.d")).toEqual(["a", "b", "0", "c", "d"]);
+
+  expect(toPath(0)).toEqual([0]);
+
+  const symbol = Symbol();
+
+  expect(toPath(symbol)).toEqual([symbol]);
+});
