8496f9d48a54441ee1eda8b8337b5635018cb6c5
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-30 20:13:41 +0430

    feat: Add `slice` method

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export { default as isEven } from "./is-even.js";
 export { default as isNumber } from "./is-number.js";
 export { default as isString } from "./is-string.js";
 export { default as isSymbol } from "./is-symbol.js";
+export { default as slice } from "./slice.js";
 export { default as sum } from "./sum.js";
 export { default as sumByFn } from "./sum-by-fn.js";
 export { default as sumByProperty } from "./sum-by-property.js";
