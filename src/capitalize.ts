1bf699eeed72126638ffe007ca853785ab676566
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-31 02:54:20 +0430

    feat: Add `capitalize` method

---
+++ src/capitalize.ts
@@ -0,0 +1,20 @@
+import lowerCase from "./lower-case.js";
+import upperFirst from "./upper-first.js";
+
+/**
+ * It will convert the first character of the `string` to uppercase and the remaining to lowercase.
+ * @since 1.0.0
+ * @param string
+ * @example
+ * capitalize('fred');
+ * // => 'Fred'
+ * @example
+ * capitalize('FRED');
+ * // => 'Fred'
+ * @example
+ * capitalize('frED');
+ * // => 'Fred'
+ */
+export default function capitalize<Value extends string>(string: Value): Capitalize<Lowercase<Value>> {
+  return upperFirst(lowerCase(string));
+}
