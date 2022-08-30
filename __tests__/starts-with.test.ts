a9a0b7df44c8025f89e2c74895288822a996e52f
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-31 02:29:33 +0430

    feat: Add `startsWith` method (#59)

---
+++ __tests__/starts-with.test.ts
@@ -0,0 +1,9 @@
+import { startsWith } from "@extremejs/utils";
+
+it("should check whether the string starts with the given searchString string", () => {
+  expect(startsWith("012345", "012")).toBe(true);
+
+  expect(startsWith("012345", "12")).toBe(false);
+
+  expect(startsWith("012345", "234", 2)).toBe(true);
+});
---
+++ src/starts-with.ts
@@ -0,0 +1,19 @@
+/**
+ * It will check whether the `string` starts with the given `searchString`.
+ * @since 1.0.0
+ * @param string
+ * @param searchString
+ * @param [position=0]
+ * @example
+ * startsWith("012345", "012");
+ * // => true
+ * @example
+ * startsWith("012345", "12");
+ * // => false
+ * @example
+ * startsWith("012345", "234", 2);
+ * // => true
+ */
+export default function startsWith(string: string, searchString: string, position?: number): boolean {
+  return string.startsWith(searchString, position);
+}
