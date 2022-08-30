ba8ff6459afda68b782272db8d78306b6b2cdaed
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-31 02:48:32 +0430

    feat: Add `lowerFirst` method (#61)

---
+++ src/lower-first.ts
@@ -0,0 +1,21 @@
+import first from "./first.js";
+import lowerCase from "./lower-case.js";
+import tail from "./tail.js";
+
+/**
+ * It will convert the first character of the `string` to lowercase.
+ * @since 1.0.0
+ * @param string
+ * @example
+ * lowerFirst('Fred');
+ * // => 'fred'
+ * @example
+ * lowerFirst('FRED');
+ * // => 'fRED'
+ * @example
+ * lowerFirst('fred');
+ * // => 'fred'
+ */
+export default function lowerFirst<Value extends string>(string: Value): Uncapitalize<Value> {
+  return (lowerCase(first(string) ?? "") + tail(string)) as Uncapitalize<Value>;
+}
