90dab4db811dd7050ccae8afae0235cbe16af2fb
Ardalan Amini <ardalanamini22@gmail.com>
2022-08-31 15:40:48 +0430

    feat: Add `mean` method (#97)

---
+++ src/mean.ts
@@ -0,0 +1,13 @@
+import sum from "./sum.js";
+
+/**
+ * It will calculate the mean of the provided `numbers`.
+ * @since 1.0.0
+ * @param numbers
+ * @example
+ * mean([1, 2, 3]);
+ * // => 2
+ */
+export default function meanByFn(numbers: number[]): number {
+  return sum(numbers) / numbers.length;
+}
