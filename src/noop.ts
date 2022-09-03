9f6cb9b0741a289e0a58011aae61515bd09a7511
Ardalan Amini <ardalanamini22@gmail.com>
2022-09-03 19:17:57 +0430

    feat: Add `noop` method (#146)

---
+++ src/noop.ts
@@ -0,0 +1,11 @@
+/**
+ * This method returns `undefined`.
+ * @group Other
+ * @since 1.0.0
+ * @example
+ * noop();
+ * // => undefined
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function noop(): void {
+}
