e027d335400aa1a9164572b8b14267934984cff6
Ardalan Amini <ardalanamini22@gmail.com>
2023-02-20 19:37:05 +0330

    docs: improve `divide` method documents
    Signed-off-by: Ardalan Amini <ardalanamini22@gmail.com>

--- src/divide.ts
+++ src/divide.ts
@@ -4,7 +4,7 @@
  * @since 1.0.0
  * @param dividend The first number in a division.
  * @param divisor The second number in a division.
- * @returns the quotient.
+ * @returns The quotient.
  * @example
  * divide(5, 2);
  * // => 2.5
