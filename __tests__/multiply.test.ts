3901133c1fde86403dc3bdd4ae8b83a644e752f1
Ardalan Amini <ardalanamini22@gmail.com>
2023-02-20 18:38:20 +0330

    feat: add `multiply` method
    Signed-off-by: Ardalan Amini <ardalanamini22@gmail.com>

---
+++ __tests__/multiply.test.ts
@@ -0,0 +1,5 @@
+import { multiply } from "@extremejs/utils";
+
+it("should multiply two numbers", () => {
+  expect(multiply(5, 2)).toBe(10);
+});
---
+++ src/multiply.ts
@@ -0,0 +1,13 @@
+/**
+ * Multiply two numbers..
+ * @group Math
+ * @since 1.0.0
+ * @param multiplier
+ * @param multiplicand
+ * @example
+ * multiply(5, 2);
+ * // => 10
+ */
+export default function multiply(multiplier: number, multiplicand: number): number {
+  return multiplier * multiplicand;
+}
