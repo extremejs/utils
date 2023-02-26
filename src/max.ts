90db84fae2fc46e22e5e699d07a7803b995a3ccf
Ardalan Amini <ardalanamini22@gmail.com>
2023-02-26 18:16:07 +0330

    feat: add `max` method
    Signed-off-by: Ardalan Amini <ardalanamini22@gmail.com>

---
+++ src/max.ts
@@ -0,0 +1,15 @@
+import maxByFn from "./max-by-fn.js";
+
+/**
+ * Computes the `maximum` of the `numbers`.
+ * @group Math
+ * @since 1.0.0
+ * @param numbers The array to iterate over.
+ * @returns The maximum value.
+ * @example
+ * max([1, 2, 3]);
+ * // => 3
+ */
+export default function max(numbers: number[]): number {
+  return maxByFn(numbers, number => number);
+}
